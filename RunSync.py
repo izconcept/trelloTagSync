f9f79c570c55294aa5b7fc05d42b493fc5b1494e
izconcept <IzKevin@Kevins-MacBook-Pro.local>
2017-01-20 21:47:09 -0500

    API optimizations + improved graph printing
    -Optimized API calls to all possible labels used.
    
    -Optimized API calls to only return relevant data.
    
    -Improved graph printing function
    
    -Added comments

--- RunSync.py
+++ RunSync.py
@@ -1,30 +1,22 @@
-import urllib, json
-import graph
-from difflib import SequenceMatcher as stringMatcher
-# HTTP call to list1
+import urllib, json, graph
 
-url = "https://api.trello.com/1/lists/588288bcca786b868bf78a9c/cards?key=802bfaebca5c1949a376ae0fa56eec6e&token=4d0d125517f9dccfd1838b32ebb1fa732b196bfcf9bdd26a66a2e3c3bac1c921"
-response = urllib.urlopen(url)
-list1 = json.loads(response.read())
-
-url = "https://api.trello.com/1/lists/588289e01f0045d1075ff7e0/cards?key=802bfaebca5c1949a376ae0fa56eec6e&token=4d0d125517f9dccfd1838b32ebb1fa732b196bfcf9bdd26a66a2e3c3bac1c921"
-response2 = urllib.urlopen(url)
-list2 = json.loads(response2.read())
-
-# Iterating through both lists and storing labels in LOWERCASE in an array/list
 tagList = []
 idList = []
-for listItem in list1:
-    for label in listItem["labels"]:
-        if label["id"] not in idList:
-            tagList.append(label["name"].lower())
-            idList.append(label["id"])
 
-for listItem in list2:
-    for label in listItem["labels"]:
-        if label["id"] not in idList:
-            tagList.append(label["name"].lower())
-            idList.append(label["id"])
+# Get list of board IDs
+response = urllib.urlopen("https://api.trello.com/1/members/5882880821001df768e97ab1/boards?fields=id&key=802bfaebca5c1949a376ae0fa56eec6e&token=4d0d125517f9dccfd1838b32ebb1fa732b196bfcf9bdd26a66a2e3c3bac1c921")
+boardList = json.loads(response.read())
 
+# Get list of labels/tags in each board and store them in tagList
+for board in boardList:
+    response = urllib.urlopen("https://api.trello.com/1/boards/"+board["id"]+"/cards?fields=labels&key=802bfaebca5c1949a376ae0fa56eec6e&token=4d0d125517f9dccfd1838b32ebb1fa732b196bfcf9bdd26a66a2e3c3bac1c921")
+    labelList = json.loads(response.read())
+    for labels in labelList:
+        for label in labels['labels']:
+            if label["id"] not in idList and label.has_key("name"):
+                tagList.append(label["name"].lower())
+                idList.append(label["id"])
+
+# Declaring my graph data structure and storing my tags inside it
 tagGraph = graph.simpleTagGraph(tagList)
 tagGraph.printGraph()
--- graph.py
+++ graph.py
@@ -1,8 +1,10 @@
+# A simple adjacency matrix graph data structure
 from difflib import SequenceMatcher as stringMatcher
 class simpleTagGraph:
     nodes = {}
     weights = []
 
+    # Initialize the weights array and automatically generate and store the sequenceMatcher similarity as weights
     def __init__(self, tagList):
         self.weights = [[0] * len(tagList) for i in range(len(tagList))]
         for x in range(0, len(tagList)):
@@ -13,11 +15,13 @@ class simpleTagGraph:
                 else:
                     self.weights[x][y] = stringMatcher(None, tagList[x], tagList[y]).ratio()
 
+    # Returns the set of weights corresponding to a node
     def getWeights(self, tagName):
         return self.weights[self.nodes[tagName]]
 
+    # Prints the adjacency matrix graph
     def printGraph(self):
-        for row in self.weights:
-            for item in row:
-                print "| %.3f" %(item),
-            print
+        row_format = "{:>18}" * (len(self.nodes) + 1)
+        print row_format.format("", *self.nodes)
+        for tagName, row in zip(self.nodes, self.weights):
+            print row_format.format(tagName +" |", *row)
