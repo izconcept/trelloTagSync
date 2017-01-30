4c364bad948041c0d8745af5c00a61c449fe7e52
izconcept <izconcept@users.noreply.github.com>
2017-01-30 14:46:56 -0500

    Added OAuth
    Implemented Oauth using Trello supplied driver.
    Making calls from front-end.

--- tagHandler.js
+++ tagHandler.js
@@ -1,8 +1,8 @@
 /**
- * Created by IzKevin on 2017-01-26.
+ * Created by Kevin Zhang on 2017-01-26.
  */
 
-var tagList = [["Bug", "bugs", "bog"],["product launch", "Product-launch", "launch product"],["error", "erreur"]];
+var tagList = {};
 
 function displayTags(tag) {
     for(var i = 0; i < tag.length; i++) {
@@ -18,13 +18,50 @@ function displayTags(tag) {
 }
 
 
-function groupBy(tag) {
-    
+function retrieveLabels(boards) {
+    tagList = []
+    boards.forEach(function(board) {
+        Trello.get('/boards/'+board['id']+'/cards?fields=labels', function(response) {
+            response.forEach(function(card) {
+                card["labels"].forEach(function(label) {
+                    tagList[label['id']] = label['name']
+                })
+            })
+        }, function(response) {
+            console.log("Error Retrieving Labels")
+        });
+    })
+    console.log(tagList);
+}
+
+function retrieveTags() {
+    Trello.get('/member/me/boards?fields=id', function(response) {
+        retrieveLabels(response);
+    }, function() {
+        console.log("Error Retrieving Tags");
+    });
 }
 
 $(document).ready(function() {
+    Trello.authorize({
+      type: 'popup',
+      name: 'TrelloSync Login',
+      scope: {
+        read: 'true',
+        write: 'true' },
+      expiration: 'never',
+      success: function(response) {
+          $("#login").remove();
+          retrieveTags();
+      },
+      error: function(response) {
+          console.log("Error");
+      }
+    });
+    /*
     displayTags(tagList);
     $("#tags").on("click", ".tag-unique", function() {
         $(this).closest(".tag-group").children(".group-by").html("<span class='group-by-button' onclick='groupBy(this)'>Group by: "+$(this).html()+"</span>");
     })
+    */
 })
