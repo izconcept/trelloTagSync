f19548cb3541f22931521c4d40767476d16f96c3
izconcept <izconcept@users.noreply.github.com>
2017-02-01 09:32:10 -0500

    Functionning DELETE + POST
    Implemented functionality that allows users to replace a cluster of
    tags by a single tag.
    DELETEs redundant tags and POSTs the tag that is wanted.

--- tagHandler.js
+++ tagHandler.js
@@ -1,16 +1,13 @@
-/**
- * Created by Kevin Zhang on 2017-01-26.
- */
-
-var tagList = {};
 var cardList = {};
+var tagCluster;
 
 $(document).ready(function() {
     //Call login script
     authorize();
-    //displayTags(tagList);
-    $("#tags").on("click", ".tag-unique", function() {
-        $(this).closest(".tag-group").children(".group-by").html("<span class='group-by-button' onclick='groupBy(this)'>Group by: "+$(this).html()+"</span>");
+    $(".container").on("click", ".tag-unique", function() {
+        var id = $(this).data("tagid");
+        var clusterNumber = $(this).data("group");
+        $(this).closest(".tag-group").children(".group-by").html("<span class='group-by-button' onclick='combineAndRemoveTags(&quot;" + id + "&quot;,"+clusterNumber+")'>Group by: "+$(this).html()+"</span>");
     })
 
 })
@@ -42,6 +39,8 @@ function authorize() {
 //Makes the necessary API calls to retrieve all the labels from the user
 function retrieveTags() {
     Trello.get('/member/me/boards?fields=id', function(boards) {
+        var dataString = "";
+        var tagList = {};
         boards.forEach(function(board) {
             Trello.get('/boards/'+board['id']+'/cards?fields=labels', function(response) {
                 var counter = 0;
@@ -49,12 +48,15 @@ function retrieveTags() {
                     counter++;
                     cardList[card['id']] = [];
                     card["labels"].forEach(function(label) {
-                        tagList[label['id']] = label['name'];
+                        if(!tagList[label['id']]){
+                            dataString += label['id'] + "=" + label['name'] + "&";
+                        }
+                        tagList[label['id']] = label['name']
                         cardList[card['id']].push(label['id']);
+                        if(counter == 10) {
+                            generateTagClusters(dataString);
+                        }
                     });
-                    if(counter >= response.length) {
-                        generateTagClusters();
-                    }
                 })
             }, function(response) {
                 console.log("Error Retrieving Labels: " + response)
@@ -66,10 +68,14 @@ function retrieveTags() {
 }
 
 //Given a tag ID, replaces all the closest tags with the new one
-function combineAndRemoveTags(IDtoReplace, cluster) {
+function combineAndRemoveTags(IDtoReplace, clusterNumber) {
+    var cluster = tagCluster[clusterNumber];
     Object.keys(cardList).forEach(function (cardID) {
         cardList[cardID].forEach(function(labelID) {
-            if(cluster[labelID]) {
+            if(cluster[labelID] && labelID != IDtoReplace) {
+                console.log("Removed: " + labelID);
+                console.log("Added: " + IDtoReplace);
+                /*
                 Trello.post('/cards/'+cardID+'/idLabels/',{value: IDtoReplace},
                     function(response) {
                         console.log("Tag added: " + response);
@@ -82,35 +88,40 @@ function combineAndRemoveTags(IDtoReplace, cluster) {
                     }, function(response) {
                         console.log("Error: " + response);
                     });
+                    */
             }
         });
     });
 }
 
-//Primitive tag grouping function
-function generateTagClusters() {
+//Calls python clustering functionality
+function generateTagClusters(dataString) {
+    console.log(dataString)
     $.ajax({
         url: "groupTags.py",
-        data: JSON.stringify(tagList),
         type: "POST",
-        dataType: "json",
+        data: dataString,
         success: function(response) {
+            console.log("SUCCESS");
+            tagCluster = response;
             console.log(response);
+            displayTags(response);
         },
         error: function(response) {
+            console.log("ERROR");
             console.log(response);
         }
     })
 }
 
 function displayTags(tag) {
+    $(".container").append('<h1 class="lead">Grouped tags</h1><div id="tags"></div>')
     for(var i = 0; i < tag.length; i++) {
         $("#tags").append("<div class='tag-group' id='group-"+i+"'></div>");
-        tag[i].forEach(function(uniqueTag) {
-            console.log(uniqueTag)
-            $("#group-"+i).append("<span class='tag-unique'>"+uniqueTag+"</span>");
+        Object.keys(tag[i]).forEach(function(id) {
+            $("#group-"+i).append("<span class='tag-unique' data-group='"+i+"' data-tagid='"+id+"'>"+tag[i][id]+"</span>");
         })
-        if(tag[i].length > 1) {
+        if(Object.values(tag[i]).length > 1) {
             $("#group-"+i).append("<p class='group-by' id='group-by-'"+i+"></p>");
         }
     }
